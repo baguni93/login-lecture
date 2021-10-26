97a4beb671efb723cec051db6df147b716b041e9
baguni <wls8428@gmail.com>
2021-10-26 12:18:50 +0900

    프론트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -12,6 +12,7 @@ app.set("views", "./src/views"); //views는 ./views 파일이다.
 app.set("view engine", "ejs"); //views 폴더 안 html 코드들을 view 엔진의 ejs로 해석해 줄 것이다.
 //ejs는 굉장한 많이 사용하는 view 엔진 중 하나인데, html 이랑 비슷하다. ? 
 
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); //use -> 미들 웨어를 등록해주는 메서드. 
 
