ed5e2c6d44ac037ac7b629db33b657e89cff40fc
baguni <wls8428@gmail.com>
2021-10-29 15:10:14 +0900

    회원 가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,23 +1,27 @@
 "use strict";
 
 const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
     psword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("#button");
+    confirmPsword = document.querySelector("#confirm-psword"),
+    registerBtn = document.querySelector("#button");
 
 
 
-console.log(id);
-console.log("hello");
 
-loginBtn.addEventListener("click", login);
+registerBtn.addEventListener("click", register);
 
-function login() {
+function register() {
    const req = {
        id : id.value,
+       name: name.value,
        psword : psword.value,
+       confirmPsword : confirmPsword.value,
    };
 
-   fetch("/login",{
+   console.log(req);
+
+   fetch("/register",{
        method: "Post",
        headers: {
            "Content-Type": "application/json",
@@ -27,11 +31,11 @@ function login() {
     .then((res) => res.json())
     .then((res) => {
         if(res.success){
-            location.href = "/";
+            location.href = "/login";
         }else{
             alert(res.msg);
         }
     }).catch((err) =>{
-        console.error(new Error("로그인 중 에러 발생"));
+        console.error(new Error("회원가입 중 에러 발생"));
     });
 }
